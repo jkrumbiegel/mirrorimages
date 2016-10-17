9c35b5fea64f4f7ee529ec388dc613b0f245b667
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2016-10-17 18:52:16 +0200

    replaced variance calculation code with concise one-liner

--- imageStackVariance.m
+++ imageStackVariance.m
@@ -1,23 +1,11 @@
 function out = imageStackVariance(stack)
 % The image stack needs to be a 4D array with dimensions x,y,color,n
 
-if ~isfloat(stack)
-    stack = double(stack);
-end
-
-nImages = size(stack,4);
-
 % Calculate means of all color channels at each pixel over all images in
-% the stack
-means = mean(stack,4);
-
-% Calculate the difference of each color value from its stack mean.
-% Indexing out of bounds for the means array is equivalent to performing a
-% slower repmat call
-diffsFromMean = stack - means(:,:,:,ones(nImages,1));
-
+% the stack and calculate the difference of each color value from its stack mean.
 % Square differences and take the mean, this is the variance
-out = mean(diffsFromMean.^2,4);
+out = mean(bsxfun(@minus,double(stack),mean(double(stack),4)).^2,4);
+
 
 
     
