fd2d65e2afd4615fd28c8accae71937cf7b2dd47
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2016-10-17 18:28:25 +0200

    minor additions

--- rescaleImageFloatsToUINT8.m
+++ rescaleImageFloatsToUINT8.m
@@ -3,9 +3,13 @@ function out = rescaleImageFloatsToUINT8(im)
 if ndims(im) == 2
     maximum = max(max(im));
     minimum = min(min(im));
+    out = uint8(((im-minimum)./maximum).*255);
 elseif ndims(im) == 3
-    maximum = max(max(max(im)));
-    minimum = min(min(min(im)));
+    out = uint8(zeros(size(im)));
+    for i=1:size(im,3)
+        maximum = max(max(im(:,:,i)));
+        minimum = min(min(im(:,:,i)));
+        out(:,:,i) = uint8(((im(:,:,i)-minimum)./maximum).*255);
+    end
 end
 
-out = uint8(((im-minimum)./maximum).*255);
